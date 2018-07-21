b663e8385281783024702373d31c893828a187ab
canamika27 <41502651+canamika27@users.noreply.github.com>
2018-07-21 13:11:07 +0530

    Create Chatbot.py
    Backend Python code using Chatterbot

---
+++ Chatbot.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app = Flask(__name__)
+
+chatbot = ChatBot(
+    'Default Response Example Bot',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch'
+        },
+        {
+            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+            'threshold': 0.65,
+            'default_response': 'I am sorry, but I do not understand.'
+        },
+          'chatterbot.logic.MathematicalEvaluation'
+    ],
+    trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
+)
+
+
+# Train based on the english corpus
+chatbot.train("chatterbot.corpus.english")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(english_bot.get_response(userText))
+
+
+if __name__ == "__main__":
+    app.run()
